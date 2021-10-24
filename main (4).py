76d5137966c17d10eb3705e045528d21e3311fad
AntonMarinin <83450695+AntonMarinin@users.noreply.github.com>
2021-10-24 14:03:25 +0300

    inv_script_lesson3

---
+++ main (4).py
@@ -0,0 +1,16 @@
+string = "Hello, World!"
+string2 = "- Hello to you!"
+string3 = string + '' + string2
+count = len(string)
+print(f"Count symbols: {count}")
+print(string3)
+print((string + "")*3)
+string = "I aM a StrinG aNd i wAnt To Be pRinTed"
+print(string.upper())
+print(string.lower())
+print(string.capitalize())
+print(string.title())
+string = "Hello world!"
+print(string[8])
+print(string[-1])
+print(string[3:7])
